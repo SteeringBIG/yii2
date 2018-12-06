fd6463ab712c32e05a6e764fc412c88f05397d9f
SteeringBIG <arway@inbox.ru>
2018-12-07 00:48:32 +0700

    Создание миграций. Таблица пользователей. Добавление пользователей по умолчанию.

---
+++ migrations/m181206_150207_AddTableUser.php
@@ -0,0 +1,51 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m181206_150207_AddTableUser
+ */
+class m181206_150207_AddTableUser extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+		$this->createTable('user', [
+			'id' => $this->primaryKey()->comment('Первичный ключь'),
+			'user_id' => $this->integer()->unsigned()->comment('Код пользователя во внешней базе'),
+			'status' => $this->bigInteger()->unsigned()->notNull()->defaultValue(1)->comment('Статус пользователя в системе'),
+			'login' => $this->string(50)->notNull()->unique()->comment('Логин пользователя'),
+			'password_hash' => $this->string(200),
+			'last_name' => $this->string(50)->comment('Фамилия'),
+			'name' => $this->string(50)->comment('Имя'),
+			'e_mail' => $this->string(50)->comment('Электронная почта'),
+			'phone' =>$this->string(10)->comment('Телефон')
+		]);
+		
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+		$this->dropTable('user');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m181206_150207_AddTableUser cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
---
+++ migrations/m181206_172537_InsertDefaultUser.php
@@ -0,0 +1,49 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m181206_163115_InsertUser
+ */
+class m181206_172537_InsertDefaultUser extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+	    $this->insert('user', [
+	    	'login' => 'Admin',
+		    'name' => 'Administrator'
+	    ]);
+	
+	    $this->insert('user', [
+	    	'login' => 'User',
+		    'name' => 'Test user'
+	    ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+		$this->delete('user', 'login=Admin');
+		$this->delete('user', 'login=User');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m181206_163115_InsertUser cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
